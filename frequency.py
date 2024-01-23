a8abf4ba28cbf1aa6fa348087e21a257f6b435a2
Manthan Kulkarni <68463944+ManthanK-24@users.noreply.github.com>
2024-01-23 09:49:06 +0530

    Python Program for frequency of numbers

---
+++ frequency.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    ans = 0
+    freq = {}
+    for key in ar:
+        if key in freq:
+            freq[key] += 1
+        else:
+            freq[key] = 1
+    for key, val in freq.items():
+        ans += val//2
+    return ans
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
